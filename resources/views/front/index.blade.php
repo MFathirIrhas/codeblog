acdfb370296566c42e56131dab47f1eabf1252b9
MFathirIrhas <fathir.irhas@outlook.com>
2016-03-02 01:35:05 +0700

    Add some contents

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -14,8 +14,37 @@
                 </blockquote>
 			</div>
 		</div>
-	</div>
 
+	</div>
+	</br>
+	</br>
+	</br>
+	</br>
+	<div class="row">
+		<div class="box">
+			<hr>
+			<h1 class="intro-text text-center"><strong><span class="label label-success">&#8659 Latest Posts &#8659 </span></strong></h1>
+			<hr>
+			@foreach($posts as $post)
+            <div class="box">
+                <div class="col-lg-12 text-center">
+                    <h2>{{ $post->title }}
+                    <br>
+                    <small>{{ $post->user->username }} {{ trans('front/blog.on') }} {!! $post->created_at . ($post->created_at != $post->updated_at ? trans('front/blog.updated') . $post->updated_at : '') !!}</small>
+                    </h2>
+                </div>
+                <div class="col-lg-12">
+                    <p>{!! $post->summary !!}</p>
+                </div>
+                <div class="col-lg-12 text-center">
+                    {!! link_to('blog/' . $post->slug, trans('front/blog.button'), ['class' => 'btn btn-primary btn-sm']) !!}
+                    <hr>
+                </div>
+            </div>
+        	@endforeach
+  		</div>
+ 	</div>
+	
 @stop
 
 
